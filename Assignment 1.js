1acf07a37caf4762671f16d453674bca65584333
Jkatu <141175580+Jkatu@users.noreply.github.com>
2025-09-15 18:28:28 +0300

    Update Assignment 1.js

---
+++ Assignment 1.js
@@ -0,0 +1,13 @@
+// Create a variable for my name
+let name = "Rhoda";
+
+// Create a variable for my age
+let age = 20;
+
+// Create an array of my 3 favorite  foods
+let favoriteFoods = ["Pizza", "Ice Cream", "Sushi"];
+
+// Print them out
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Foods:", favoriteFoods);
