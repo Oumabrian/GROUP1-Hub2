4f0b5535399320808484625ce8743774a73cbae6
erick ocharo <erickocharo134@gmail.com>
2025-09-16 13:08:21 +0300

    assignment

---
+++ assigment1.py
@@ -0,0 +1,11 @@
+#myname
+student_name = "erick ocharo"
+print("my name is;", student_name)
+
+#age
+age =22
+print("my age is :",age)
+
+#favourite toys 
+favourite_toys ={"fitness tracker","portable projector ","Adult LEGO"}
+print("my favourite toys are :",favourite_toys)
