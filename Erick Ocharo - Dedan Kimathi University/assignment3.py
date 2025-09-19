721db9cc56d98ae3aa578e9d41fb12d5bdc2f70a
erick ocharo <erickocharo134@gmail.com>
2025-09-19 18:08:57 +0300

    assignments

---
+++ Erick Ocharo - Dedan Kimathi University/assignment3.py
@@ -0,0 +1,13 @@
+
+# make a function introduce (name,age)  that prints  ("hi ,my name is (name) and i am (age) years ld")
+def introduce(name,age):
+
+    print("Hi ,my name is {name} and i am {age} years old ")
+    
+    
+    #make a function multiply (a,b)that returns the product 
+    def multiply(a, b):
+        return a*b
+    
+    
+    
