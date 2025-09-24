6828b05930da161e08735228fe7ae080546b0d30
unknown <abigaelnyambura73@gmail.com>
2025-09-24 15:00:38 +0300

    initial commit

--- assignment1.js
+++
@@ -1,5 +0,0 @@
-let name = "Abigael";
-let age = 20;
-let toys = ["doll", "car", "puzzle"];
-console.log("My name is " + name + " and I am " + age + " years old.");
-console.log("My favorite toys are: " + toys.join(", ") + ".");
